4f584fdc1393f334b312e7dc87c3d7a0ca8423b9
Sumukh Kowndinya <112752323+Sumz-K@users.noreply.github.com>
2024-03-09 14:01:30 +0530

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int a[]={0,2,0,0,2,0,0,3,0,0};
+    for(int i=1;i<8;i++){
+        cout<<a[i]<<endl;
+        a[i+1]=a[i]+a[i-1];
+    }
+
+}
